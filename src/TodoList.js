749e67f1baa4806b69114c6480e6410678378d58
Tienbot <tien.ru@mail.ru>
2024-10-05 02:56:38 +0300

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,56 @@
+import React, {Component} from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: []
+        };
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    addItem(e) {
+        var itemArray = this.state.items;
+        if (this._inputElement.value!== "") {
+            itemArray.unshift({
+              text: this._inputElement.value,
+              key: Date.now()
+            });
+            this.setState({
+              items: itemArray
+        }   );
+            this._inputElement.value = "";
+        }
+        
+        console.log(itemArray);
+        e.preventDefault();
+    }
+
+    deleteItem(key) {
+        var fiteredItems = this.state.items.filter(function(item) {
+          return (item.key!== key);
+        });
+        this.setState({
+          items: fiteredItems
+        }); 
+    }
+   
+
+    render() {
+        return (
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a} placeholder="введите задачу"></input>
+                        <button type="submit">ok</button>
+                    </form>
+                </div>
+                <TodoItems entries={this.state.items} delete={this.deleteItem}/>
+            </div> 
+        );
+    } 
+}
+export default TodoList;
